831e5cc52f482d9db8846ebaebecd358efa1ab8b
Sab <sab@pop-os.localdomain>
2023-01-29 20:18:24 +0500

    changing format from png to webp

--- web/src/widget-api.js
+++ web/src/widget-api.js
@@ -60,7 +60,7 @@ export function sendSticker(content) {
 	const widgetData = {
 		...data,
 		description: content.body,
-		file: `${content.id}.png`,
+		file: `${content.id}.webp`,
 	}
 	// Element iOS explodes if there are extra fields present
 	delete widgetData.content["net.maunium.telegram.sticker"]
